10ff1da11139c6ac62844f5023eddc7447e75fe9
SSHariharan24 <hariabi2004a@gmail.com>
2025-07-11 17:26:30 +0530

    latest commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,24 @@ import { app, server } from "./lib/socket.js";
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ limit: '500mb', extended: true }));
 app.use(cookieParser())
+
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://real-time-chatapp-mu.vercel.app"
+];
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 dotenv.config()
 const PORT = process.env.PORT
 const __dirname = path.resolve();
